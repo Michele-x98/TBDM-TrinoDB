a7c855678221aa485c571ecd28f2763c1be75a68
ArmandoXheka <armando.xheka@omnes.cloud>
2023-03-10 11:16:44 +0100

    Setted all ready to permorm all queries

--- kafka-producer/index.js
+++ kafka-producer/index.js
@@ -2,72 +2,72 @@ const git = require("./services/useGithub");
 const kafka = require("./services/useKafka");
 const { Kafka } = require("kafkajs");
 
-const userName = "ArmandoXheka";
-// Michele-x98, flaviopopoff,ArmandoXheka
+const mainUsers = ["ArmandoXheka", "Michele-x98", "flaviopopoff"];
 
-let userPropertiesToGet = ["name", "login", "bio", "avatar_url"];
-let reposPropertiesToGet = [
-  "name",
-  "description",
-  "url",
-  "created_at",
-  "stars",
-  "forks",
-  "language",
-  "open_issues",
-  "topics",
-  "license",
-];
-let followerPropertiesToGet = ["login", "id", "type"];
-let followingPropertiesToGet = ["login", "id", "type"];
+let userPropertiesToGet = ["name", "login", "avatar_url", "type"];
+let reposPropertiesToGet = ["name", "description", "url", "created_at", "stars", "forks", "language", "open_issues", "topics", "license"];
+let followerPropertiesToGet = ["login"];
+let followingPropertiesToGet = ["login"];
 
 // FUNCTION THAT SELECTS ONLY THE FIELD REQUIRED
 // IF VALUE ISN'T INSIDE THE OBJECT IT PASS AWAY WITH NO ERRORS
-function pickOnlySelectedValue(obj, ...props) {
-  return props.reduce(function (result, prop) {
-    if (obj[prop]) result[prop] = obj[prop];
-    return result;
-  }, {});
+function pickOnlySelectedValue(obj) {
+	return obj.login;
 }
 
 // GETUSER AND SELECT FIELDS REQUESTED
 const getUser = async (username) => {
-  let res = await git.fetchGetGithubUser(username);
-  return pickOnlySelectedValue(res, ...userPropertiesToGet);
+	return await git.fetchGetGithubUser(username);
 };
 
 const getUserRepos = async (username) => {
-  let res = await git.fetchGetGithubUserRepos(username);
-  return res.map((el) => pickOnlySelectedValue(el, ...reposPropertiesToGet));
+	return await git.fetchGetGithubUserRepos(username);
 };
 
 const getFollowers = async (username) => {
-  let res = await git.fetchGetGithubUserFollowers(username);
-  return res.map((el) => pickOnlySelectedValue(el, ...followerPropertiesToGet));
+	let res = await git.fetchGetGithubUserFollowers(username);
+	return res.map((el) => pickOnlySelectedValue(el, ...followerPropertiesToGet));
 };
 
 const getFollowings = async (username) => {
-  let res = await git.fetchGetGithubUserFollowings(username);
-  return res.map((el) =>
-    pickOnlySelectedValue(el, ...followingPropertiesToGet)
-  );
+	let res = await git.fetchGetGithubUserFollowings(username);
+	return res.map((el) => pickOnlySelectedValue(el, ...followingPropertiesToGet));
 };
 
 // INIT
-const init = async (username) => {
-  const promises = [
-    getUser(username),
-    // getUserRepos(username),
-    // getFollowers(username),
-    // getFollowings(username),
-  ];
-  let [user] = await Promise.all(promises);
-  // repos.forEach((element) => (element.login = user.login));
+const init = async () => {
+	console.time();
 
-  let topic = "users";
+	let users = mainUsers;
+	// GET ALL FOLLOWERS FOR ALL THE USERS ON LIST AND TRANSFORM ALL IN AN ARRAY OF OBJECTS
+	const followersPromises = users.map((name) => getFollowers(name));
+	let followers = (await Promise.all(followersPromises)).flat(Infinity);
+	console.log("Followers  --> ", followers.length);
 
-  console.log(user);
-  kafka.runKafka(topic, user);
+	// GET ALL FOLLOWINGS FOR ALL THE USERS ON LIST AND TRANSFORM ALL IN AN ARRAY OF OBJECTS
+	const followingsPromises = users.map((name) => getFollowings(name));
+	let followings = (await Promise.all(followingsPromises)).flat(Infinity);
+	console.log("Followings  --> ", followings.length);
+
+	// REMOVE REDUNDANCY. DELETE ALLA USERS THAT APPEAR MORE THAN ONCE
+	users = [...new Set([...users, ...followers, ...followings])];
+	console.log("Filtered  --> ", users.length);
+
+	// GET THE PROFILE OF ALL USERS
+	let profilesPromises = users.map((name) => getUser(name));
+	let profiles = await Promise.all(profilesPromises);
+	// console.log("Profiles  --> ", profiles.length);
+
+	// GET ALL REPOS OF EVERY USERS AND TRANSFORM THE ARRAYS OF OBJECTS IN A SINGLE ARRAY OF OBJECTS
+	let reposPromises = users.map((name) => getUserRepos(name));
+	let repos = (await Promise.all(reposPromises)).flat(Infinity);
+	console.log("Repos --> ", repos.length);
+
+	console.timeEnd();
+
+	// FIRST ARGUMENT TOPIC NAME, SECOND ONE ARRAY OF OBJJECTS
+	// kafka.runKafka("users", profiles);
+	// kafka.runKafka("repos", repos);
 };
 
-init(userName);
+init();
