ebaa4986a416ecf729dcffe0dc16c945e3348a41
Flavio Pocari <flaviopopoff@popoff-2.local>
2023-03-13 18:52:22 +0100

    Reformat + fix.

--- run.sh
+++ run.sh
@@ -1,54 +1,51 @@
-#Start the docker containers
+# Start the docker containers.
 docker-compose up -d
 
-#Go to the Kafka Connect container
+# Go to the Kafka Connect container.
 docker exec -it kafka bash
 
-#Set KAFKA_HOME variable to the Kafka installation directory
+# Set KAFKA_HOME variable to the Kafka installation directory.
 KAFKA_HOME=/opt/kafka
 
-#Create users topic in Kafka
+# Create users topic in Kafka.
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper 51.103.220.68:2181 --replication-factor 1 --partitions 1 --topic users
 
-#Create repos topic in Kafka
+# Create repos topic in Kafka.
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper 51.103.220.68:2181 --replication-factor 1 --partitions 1 --topic repos
 
-#Check the topics
+# Check the topics.
 $KAFKA_HOME/bin/kafka-topics.sh --zookeeper  51.103.220.68:2181 --describe
 
-#Download the MongoDB connector
+# Download the MongoDB connector.
 wget https://d1i4a15mxbxib1.cloudfront.net/api/plugins/mongodb/kafka-connect-mongodb/versions/1.9.1/mongodb-kafka-connect-mongodb-1.9.1.zip
 
-#Unzip the connector
+# Unzip the connector.
 unzip mongodb-kafka-connect-mongodb-1.9.1.zip
 
-#Copy the connector to the Kafka Connect plugins directory
+# Copy the connector to the Kafka Connect plugins directory.
 mkdir -p /opt/kafka/plugins/mongodb-connector
 cd mongodb-kafka-connect-mongodb-1.9.1/lib
-cp -R *  /opt/kafka/plugins/mongodb-connector
+cp -R * /opt/kafka/plugins/mongodb-connector
 
-#Install nano
+# Install nano with apt.
 apk update
 apk add nano
 
-#Edit the Kafka Connect configuration file
+# Edit the Kafka Connect configuration file.
 nano /opt/kafka/config/connect-distributed.properties
 uncomment plugins..
 add plugin.path=/opt/kafka/plugins
 
-#Start Kafka Connect
+# Start Kafka Connect.
 $KAFKA_HOME/bin/connect-distributed.sh $KAFKA_HOME/config/connect-distributed.properties
 
-#Check the Kafka Connect REST API
+# Check the Kafka Connect REST API.
 curl localhost:8083/ | jq
 curl localhost:8083/connector-plugins | jq
 curl localhost:8083/connectors
 
-#Create the MongoDB connector configuration file
-nano mongodb_connector_user.json
-
-#Add the following content to the file
-{
+# Create the MongoDB connector configuration file and add the content to the file.
+echo '''{
   "name": "mongo-sink-users",
   "config": {
     "connector.class": "com.mongodb.kafka.connect.MongoSinkConnector",
@@ -60,19 +57,16 @@ nano mongodb_connector_user.json
     "database": "ghdb",
     "collection": "users"
   }
-}
+}''' >> mongodb_connector_user.json
 
-#Create the MongoDB connector
+# Create the MongoDB connector.
 curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_user.json
 
-#Check the status of the connector
+# Check the status of the connector.
 curl -s "http://localhost:8083/connectors/mongo-sink-users/status"
 
-#Create the MongoDB connector configuration file
-nano mongodb_connector_repos.json
-
-#Add the following content to the file
-{
+# Create the MongoDB connector configuration file.
+echo '''{
   "name": "mongo-sink-repos",
   "config": {
     "connector.class": "com.mongodb.kafka.connect.MongoSinkConnector",
@@ -84,12 +78,12 @@ nano mongodb_connector_repos.json
     "database": "ghdb",
     "collection": "repos"
   }
-}
+}''' >> mongodb_connector_repos.json
 
-#Create the MongoDB connector
+# Create the MongoDB connector.
 curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_repos.json
 
-#Check the status of the connector
+# Check the status of the connector.
 curl -s "http://localhost:8083/connectors/mongo-sink-repos/status"
 
 # [OPTIONAL] Consumer to check the data in the topics:
