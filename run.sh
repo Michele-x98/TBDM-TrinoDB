2df28d859c0a4d2706d40f2a44f2bf25aa62fec1
Flavio Pocari <flaviopopoff@popoff-2.local>
2023-03-15 23:32:07 +0100

    Bash script completed.

--- run.sh
+++ run.sh
@@ -1,93 +1,138 @@
-# Start the docker containers.
-sudo docker-compose up -d
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NONE='\033[0m'
 
-# Go to the Kafka Connect container.
-sudo docker exec -it kafka bash
 
+kafka_bash(){
+  COMMAND=$1
+  SLEEP_AMOUNT=$2
+  # Execute the given command inside the Kafka container.
+  docker exec -it kafka bash -c "$COMMAND"
+  # If the second argument is not empty, sleep for the given amount of time.
+  if [ ! -z "$SLEEP_AMOUNT" ]; then
+    sleep $SLEEP_AMOUNT
+  fi
+}
+
+# check if docker-compose is running, if yes, down it.
+if [ "$(docker-compose ps -q)" ]; then
+  echo "${BLUE}STOPPING THE CONTAINERS.${NONE}"
+  docker-compose down
+fi
+
+
+# Get host ip address.
+export HOST_IP="`(ifconfig en0 || ifconfig eth0) | grep inet | grep -oE "inet [0-9]+.[0-9]+.[0-9]+.[0-9]+" | awk '{print $2}'`"
+echo "\nHost IP: ${BLUE}$HOST_IP ${NONE}\n"
+
+echo "${YELLOW}STARTING CONTAINERS. ${NONE}"
+docker-compose up -d
+sleep 10
+
+#check the status of the containers.
+echo "${GREEN}CONTAINERS STARTED - STATUS ${NONE}"
+docker ps
+sleep 5
+
+echo "OPENING KAFKA SHELL AND EXPORTING ${BLUE}KAFKA_HOME${NONE}."
 # Set KAFKA_HOME variable to the Kafka installation directory.
-export KAFKA_HOME=/opt/kafka
+kafka_bash "export KAFKA_HOME=/opt/kafka"
 
-# Create users topic in Kafka.
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users
+# Create user topic.
+kafka_bash "\$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users" 10
+echo "CREATED TOPIC USER ${GREEN}SUCCESSFULLY${NONE}."
 
-# Create repos topic in Kafka.
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic repos
+# Create repos topic.
+kafka_bash "\$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic repos" 10
+echo "CREATED TOPIC REPOS ${GREEN}SUCCESSFULLY${NONE}."
 
 # Check the topics.
-$KAFKA_HOME/bin/kafka-topics.sh --zookeeper  zookeeper:2181 --describe
+echo "CHECK ${YELLOW}TOPICS${NONE}."
+kafka_bash "\$KAFKA_HOME/bin/kafka-topics.sh --zookeeper  zookeeper:2181 --describe"
 
-# Download the MongoDB connector.
-wget https://d1i4a15mxbxib1.cloudfront.net/api/plugins/mongodb/kafka-connect-mongodb/versions/1.9.1/mongodb-kafka-connect-mongodb-1.9.1.zip
 
-# Unzip the connector.
-unzip mongodb-kafka-connect-mongodb-1.9.1.zip
-
+GET_CONNECTOR="wget https://d1i4a15mxbxib1.cloudfront.net/api/plugins/mongodb/kafka-connect-mongodb/versions/1.9.1/mongodb-kafka-connect-mongodb-1.9.1.zip && unzip mongodb-kafka-connect-mongodb-1.9.1.zip"
 # Copy the connector to the Kafka Connect plugins directory.
-mkdir -p /opt/kafka/plugins/mongodb-connector
-cd mongodb-kafka-connect-mongodb-1.9.1/lib
-cp -R * /opt/kafka/plugins/mongodb-connector
+COPY_CONNECTOR="mkdir -p /opt/kafka/plugins/mongodb-connector && cd mongodb-kafka-connect-mongodb-1.9.1/lib && cp -R * /opt/kafka/plugins/mongodb-connector"
+
+# Download the MongoDB connector inside docker, uzip it and copy it inside plugins directory.
+echo "${YELLOW}DOWNLOAD MONGODB CONNECTOR ${NONE}."
+kafka_bash "$GET_CONNECTOR && $COPY_CONNECTOR" 10
 
 # Install nano with apt.
-apk update
-apk add nano
+kafka_bash "apk update && apk add nano" 10
 
-# Edit the Kafka Connect configuration file.
-nano /opt/kafka/config/connect-distributed.properties
-# Concat to "plugin.path" the following string: ",/opt/kafka/plugins"
+# Edit the Kafka Connect configuration file by adding the MongoDB connector plugin path.
+KAKFA_PLUGIN="plugin.path=/usr/local/share/java,/usr/local/share/kafka/plugins,/opt/connectors,/opt/kafka/plugins"
+kafka_bash "echo $KAKFA_PLUGIN >> /opt/kafka/config/connect-distributed.properties"
+echo "KAFKA CONFIG FILE - PLUGIN PATH ${GREEN}SETTED${NONE}.\n"
+# kafka_bash "cat /opt/kafka/config/connect-distributed.properties | grep plugin.path"
 
 # Start Kafka Connect.
-$KAFKA_HOME/bin/connect-distributed.sh $KAFKA_HOME/config/connect-distributed.properties
+echo "${YELLOW}START KAFKA CONNECTOR${NONE}.\n"
+# run a bash shell inside the kafka container and start the Kafka Connect service.
+docker exec kafka bash -c "\$KAFKA_HOME/bin/connect-distributed.sh \$KAFKA_HOME/config/connect-distributed.properties" &
+
+
+sleep 50
+
 
 # Check the Kafka Connect REST API.
-curl localhost:8083/ | jq
-curl localhost:8083/connector-plugins | jq
-curl localhost:8083/connectors
+# curl localhost:8083/ | jq
+# curl localhost:8083/connector-plugins | jq
+# curl localhost:8083/connectors
+docker exec -it kafka bash -c "curl localhost:8083/connector-plugins | jq"
+
 
 # Create the MongoDB connector configuration file and add the content to the file.
-echo '''{
-  "name": "mongo-sink-users",
-  "config": {
-    "connector.class": "com.mongodb.kafka.connect.MongoSinkConnector",
-    "topics": "users",
-    "connection.uri": "mongodb://root:example@mongo:27017",
-    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    "value.converter.schemas.enable": false,
-    "database": "ghdb",
-    "collection": "users"
+kafka_bash """echo '''{
+  \"name\": \"mongo-sink-users\",
+  \"config\": {
+    \"connector.class\": \"com.mongodb.kafka.connect.MongoSinkConnector\",
+    \"topics\": \"users\",
+    \"connection.uri\": \"mongodb://root:example@mongo:27017\",
+    \"key.converter\": \"org.apache.kafka.connect.storage.StringConverter\",
+    \"value.converter\": \"org.apache.kafka.connect.json.JsonConverter\",
+    \"value.converter.schemas.enable\": false,
+    \"database\": \"ghdb\",
+    \"collection\": \"users\"
   }
-}''' >> mongodb_connector_user.json
+}''' >> mongodb_connector_users.json"""
 
 # Create the MongoDB connector.
-curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_user.json
+kafka_bash "curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_users.json"
+sleep 5
 
 # Check the status of the connector.
-curl -s "http://localhost:8083/connectors/mongo-sink-users/status"
+kafka_bash "curl -s "http://localhost:8083/connectors/mongo-sink-users/status""
+sleep 5
+
 
 # Create the MongoDB connector configuration file.
-echo '''{
-  "name": "mongo-sink-repos",
-  "config": {
-    "connector.class": "com.mongodb.kafka.connect.MongoSinkConnector",
-    "topics": "repos",
-    "connection.uri": "mongodb://root:example@mongo:27017",
-    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    "value.converter.schemas.enable": false,
-    "database": "ghdb",
-    "collection": "repos"
+kafka_bash """echo '''{
+  \"name\": \"mongo-sink-repos\",
+  \"config\": {
+    \"connector.class\": \"com.mongodb.kafka.connect.MongoSinkConnector\",
+    \"topics\": \"repos\",
+    \"connection.uri\": \"mongodb://root:example@mongo:27017\",
+    \"key.converter\": \"org.apache.kafka.connect.storage.StringConverter\",
+    \"value.converter\": \"org.apache.kafka.connect.json.JsonConverter\",
+    \"value.converter.schemas.enable\": false,
+    \"database\": \"ghdb\",
+    \"collection\": \"repos\"
   }
-}''' >> mongodb_connector_repos.json
+}''' >> mongodb_connector_repos.json"""
 
 # Create the MongoDB connector.
-curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_repos.json
+kafka_bash "curl -s -X POST -H 'Content-Type: application/json' http://localhost:8083/connectors -d @mongodb_connector_repos.json"
+sleep 5
 
 # Check the status of the connector.
-curl -s "http://localhost:8083/connectors/mongo-sink-repos/status"
+kafka_bash "curl -s "http://localhost:8083/connectors/mongo-sink-repos/status""
 
 # [OPTIONAL] Consumer to check the data in the topics:
-$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic users --from-beginning
-$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic repos --from-beginning
+# $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic users --from-beginning
+# $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic repos --from-beginning
 
-# To shut down the containers.
-sudo docker-compose down
+echo "${GREEN}Done${NONE} ✅"
