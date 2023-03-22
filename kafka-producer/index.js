a9f7a29201b2191a4d32664fb07b891e64260030
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2023-03-22 21:08:12 +0100

    feat: [main] adds possibility to select which topic use and number of replication for repos

--- kafka-producer/index.js
+++ kafka-producer/index.js
@@ -1,6 +1,7 @@
 const git = require("./services/useGithub");
 const kafka = require("./services/useKafka");
 const { Kafka } = require("kafkajs");
+const prompt = require("prompt-sync")();
 
 const mainUsers = ["ArmandoXheka", "Michele-x98", "flaviopopoff"];
 
@@ -52,8 +53,21 @@ const getFollowings = async (username) => {
   );
 };
 
+const getTopics = () => {
+  let topic = prompt("Insert topic name between users and repos: ");
+  return ["users", "repos"].includes(topic) ? topic : getTopics();
+};
+
+const getN = () => {
+  let n = prompt("Insert the number of replication: ");
+  return n > 0 ? n : getN();
+};
+
 // INIT
 const init = async () => {
+  // GET TOPIC NAME FROM USER INPUT AND CHECK IF IT'S CORRECT, otherwise ask again
+  const topic = getTopics();
+
   console.time();
 
   let users = mainUsers;
@@ -74,21 +88,27 @@ const init = async () => {
   // GET THE PROFILE OF ALL USERS
   let profilesPromises = users.map((name) => getUser(name));
   let profiles = await Promise.all(profilesPromises);
-  // console.log("Profiles  --> ", profiles.length);
 
-  // GET ALL REPOS OF EVERY USERS AND TRANSFORM THE ARRAYS OF OBJECTS IN A SINGLE ARRAY OF OBJECTS
-  let reposPromises = users.map((name) => getUserRepos(name));
-  let repos = (await Promise.all(reposPromises)).flat(Infinity);
-  console.log("Repos --> ", repos.length);
-
-  console.timeEnd();
+  if (topic === "users") {
+    console.log("Profiles --> ", profiles.length);
+    kafka.runKafka("users", 1, profiles);
+  }
 
   // FIRST ARGUMENT TOPIC NAME, SECOND ONE ARRAY OF OBJJECTS
-  kafka.runKafka("users", profiles);
+  if (topic === "repos") {
+    // ask for the number of repos to fetch
+    let n = getN();
 
-  setTimeout(() => {
-    kafka.runKafka("repos", repos);
-  }, 2000);
+    // GET ALL REPOS OF EVERY USERS AND TRANSFORM THE ARRAYS OF OBJECTS IN A SINGLE ARRAY OF OBJECTS
+    let reposPromises = users.map((name) => getUserRepos(name));
+    let repos = (await Promise.all(reposPromises)).flat(Infinity);
+    console.log("Repos --> ", repos.length);
+    console.log("Replication --> ", n);
+
+    kafka.runKafka("repos", n, repos);
+  }
+
+  console.timeEnd();
 };
 
 init();
--- kafka-producer/services/useKafka.js
+++ kafka-producer/services/useKafka.js
@@ -2,59 +2,59 @@ const { v4: uuidv4 } = require("uuid");
 const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
-	clientId: "my-app",
-	brokers: ["192.168.198.156:9092"],
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
 });
 
 const producer = kafka.producer();
 
-const runKafka = async (topic, ...newMessages) => {
-	let messages = [];
+const runKafka = async (topic, n, ...newMessages) => {
+  let messages = [];
 
-	// ADDING KEY VALUE AS UUID
-	newMessages.forEach((element) => {
-		element.forEach((message) => {
-			messages.push({
-				key: uuidv4(),
-				value: JSON.stringify(message),
-			});
-		});
-	});
+  // ADDING KEY VALUE AS UUID
+  newMessages.forEach((element) => {
+    element.forEach((message) => {
+      messages.push({
+        key: uuidv4(),
+        value: JSON.stringify(message),
+      });
+    });
+  });
 
-	console.log(messages.length);
+  console.log(messages.length);
 
-	await producer.connect();
+  await producer.connect();
 
-	if (topic === "repos") {
-		for (let index = 0; index < 11; index++) {
-			if (messages.length > 100) {
-				let i;
-				let j;
-				let temparray;
-				let chunk = 100;
+  if (topic === "repos") {
+    for (let index = 0; index < n; index++) {
+      if (messages.length > 100) {
+        let i;
+        let j;
+        let temparray;
+        let chunk = 100;
 
-				for (i = 0, j = messages.length; i < j; i += chunk) {
-					temparray = messages.slice(i, i + chunk);
-					await producer.send({
-						topic: topic,
-						messages: temparray,
-					});
-				}
-			} else {
-				await producer.send({
-					topic: topic,
-					messages: messages,
-				});
-			}
-		}
-	} else {
-		await producer.send({
-			topic: topic,
-			messages: messages,
-		});
-	}
+        for (i = 0, j = messages.length; i < j; i += chunk) {
+          temparray = messages.slice(i, i + chunk);
+          await producer.send({
+            topic: topic,
+            messages: temparray,
+          });
+        }
+      } else {
+        await producer.send({
+          topic: topic,
+          messages: messages,
+        });
+      }
+    }
+  } else {
+    await producer.send({
+      topic: topic,
+      messages: messages,
+    });
+  }
 
-	await producer.disconnect();
+  await producer.disconnect();
 };
 
 module.exports = { runKafka };
