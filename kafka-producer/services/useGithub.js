a4911f78a959d3c31900b0dac9a3ddd10d4672fc
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2023-03-24 15:45:58 +0100

    Update useGithub.js

--- kafka-producer/services/useGithub.js
+++ kafka-producer/services/useGithub.js
@@ -1,48 +1,53 @@
-const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
-// ACCESS TOKEN ARMANDO
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+// ACCESS TOKEN
 let headers = {
-	headers: {
-		authorization: "token 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",
-	},
+  headers: {
+    authorization: "token ${YOUR_SECRET_TOKEN}",
+  },
 };
 
 let baseUrl = "https://api.github.com/users/";
 
 const fetchGetGithubUser = async (user) => {
-	return fetch(`${baseUrl}${user}`, headers).then((response) => {
-		return response
-			.json()
-			.then((data) => data)
-			.catch((err) => console.log(err));
-	});
+  return fetch(`${baseUrl}${user}`, headers).then((response) => {
+    return response
+      .json()
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  });
 };
 
 const fetchGetGithubUserRepos = async (user) => {
-	return fetch(`${baseUrl}${user}/repos`, headers).then((response) => {
-		return response
-			.json()
-			.then((data) => data)
-			.catch((err) => console.log(err));
-	});
+  return fetch(`${baseUrl}${user}/repos`, headers).then((response) => {
+    return response
+      .json()
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  });
 };
 
 const fetchGetGithubUserFollowers = async (user) => {
-	return fetch(`${baseUrl}${user}/followers`, headers).then((response) => {
-		return response
-			.json()
-			.then((data) => data)
-			.catch((err) => console.log(err));
-	});
+  return fetch(`${baseUrl}${user}/followers`, headers).then((response) => {
+    return response
+      .json()
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  });
 };
 
 const fetchGetGithubUserFollowings = async (user) => {
-	return fetch(`${baseUrl}${user}/following`, headers).then((response) => {
-		return response
-			.json()
-			.then((data) => data)
-			.catch((err) => console.log(err));
-	});
+  return fetch(`${baseUrl}${user}/following`, headers).then((response) => {
+    return response
+      .json()
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  });
 };
 
-
-module.exports = { fetchGetGithubUser, fetchGetGithubUserRepos, fetchGetGithubUserFollowers, fetchGetGithubUserFollowings };
+module.exports = {
+  fetchGetGithubUser,
+  fetchGetGithubUserRepos,
+  fetchGetGithubUserFollowers,
+  fetchGetGithubUserFollowings,
+};
